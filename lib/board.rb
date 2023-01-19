c823c8ab40c9bc9db86862a4b279dfd1982d83ae
João Paulo <jp.coutm@gmail.com>
2023-01-18 21:15:45 -0300

    Started the board class

--- lib/board.rb
+++ lib/board.rb
@@ -1,1 +1,11 @@
 # frozen_string_literal: true
+
+class Board
+  attr_accessor :dimensions, :knight_position, :knight_path
+
+  def initialize
+    @dimensions = Array.new(8) { Array.new(8, 0) }
+    @knight_position = []
+    @knight_path = []
+  end
+end
